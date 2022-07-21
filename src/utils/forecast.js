0a22b7a116e5922d5eafaf4d5539f5b0b39505d7
radovanrasha <kking1755@gmail.com>
2022-07-21 12:18:00 +0200

    final update

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require("request")
 
 const forecast = (latitude, longitude, callback)=>{
-    const url = 'http://api.weatherstack.com/current?access_key=4e4a08e4e8089bbdf788793cfb6d1d49&query=' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude) +'&units=m'
+    const url = 'http://api.weatherstack.com/current?access_key=79dd291261538ef8acfff065f343113e&query=' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude) +'&units=m'
 
 
 
@@ -12,7 +12,7 @@ const forecast = (latitude, longitude, callback)=>{
                 callback('Location doesnt exist.', undefined)
             }else{
                 
-                callback(undefined, 'We are in ' + body.location.name + '. Weather is ' + body.current.weather_descriptions[0] + '. Current temp is ' + body.current.temperature + ' but it feels like ' + body.current.feelslike + ' . ')
+                callback(undefined, 'We are in ' + body.location.name + '. Weather is ' + body.current.weather_descriptions[0] + '. Current temp is ' + body.current.temperature + ' but it feels like ' + body.current.feelslike + ' . Time zone of this city is ' + body.location.timezone_id + '. And local date/time is ' + body.location.localtime)
             }
     })
 
